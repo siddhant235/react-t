73e609af84a3510fe35f7b30240a45684a2abb98
Rut Shah <rut.shah@avoma.com>
2024-01-17 16:32:58 +0530

    u

---
+++ src/data.js
@@ -0,0 +1,83 @@
+const data = [
+  {
+    uuid: "98793e03-bcef-4c05-a24a-3dcd5a1c4475",
+    collection_type: "space",
+    external_id: "66206420",
+    name: "Test Space",
+    children: [
+      {
+        uuid: "fb1ec4a8-5b76-4bfb-a1db-9661f460dd6a",
+        collection_type: "folder",
+        external_id: "138207629",
+        name: "Test Folder",
+        children: [
+          {
+            uuid: "c3f07b9f-db8f-4f52-8a99-0881415ca118",
+            collection_type: "list",
+            external_id: "240073933",
+            name: "List",
+            children: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    uuid: "c9be00b4-820b-4762-8621-1dfa12371edb",
+    collection_type: "space",
+    external_id: "90020411089",
+    name: "Ex-spp",
+    children: [
+      {
+        uuid: "459d30d5-cef7-4ec2-b185-45f8e102ebc8",
+        collection_type: "folder",
+        external_id: "90020927822",
+        name: "CUSTOMER ISSUES",
+        children: [
+          {
+            uuid: "db1f0830-2817-4a77-989c-a6a303e47c56",
+            collection_type: "list",
+            external_id: "900201690823",
+            name: "ESCALATED",
+            children: []
+          },
+          {
+            uuid: "b018a940-e790-41e5-8301-f589cfec11de",
+            collection_type: "list",
+            external_id: "900201691344",
+            name: "support",
+            children: []
+          },
+          {
+            uuid: "056eceb4-6b9a-4edf-b5c5-d876846cbc52",
+            collection_type: "list",
+            external_id: "900201690824",
+            name: "NORMAL",
+            children: []
+          }
+        ]
+      },
+      {
+        uuid: "f5582407-8cfc-4713-87c5-140935a695ca",
+        collection_type: "list",
+        external_id: "900201691340",
+        name: "support",
+        children: []
+      },
+      {
+        uuid: "cff7cc56-5569-4be6-be84-4d2a7fc1f796",
+        collection_type: "list",
+        external_id: "900201691309",
+        name: "Adhoc List",
+        children: []
+      },
+      {
+        uuid: "fa4233db-0452-4b30-809f-a6330e96ad2d",
+        collection_type: "list",
+        external_id: "900201690790",
+        name: "List",
+        children: []
+      }
+    ]
+  }
+];
