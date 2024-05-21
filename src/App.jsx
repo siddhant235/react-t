4393a1ea52bed7d22d2f7483f1fed5b4a7ebec3a
null <null>
2024-05-21 20:03:08 +0530

    New

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <p>Avoma Frontend Interview Assignment</p>
+      <p>Crux Frontend Interview Assignment</p>
     </div>
   );
 }
